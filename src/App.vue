bdff3de9a7f55e7f22d3f6dff829edac760674cb
chawonlee <ss01066865089@gmail.com>
2024-10-28 22:37:29 +0900

    refs #1 [project setting]

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="예준 잘자!" />
+      <HelloWorld msg="첫 세팅!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
