64c2c1a8bf79340130b17ff4773f8d5a49c83b1c
chawonlee <ss01066865089@gmail.com>
2024-11-28 16:25:46 +0900

    [skills] 타이틀 추가

--- src/views/mainView/components/skillsView.vue
+++ src/views/mainView/components/skillsView.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="skills-wrap">
+    <section class="skills-title">SKILLS</section>
     <section class="swiper-container">
       <Swiper
         ref="serviceSwiper"
@@ -117,6 +118,26 @@ onBeforeUnmount(() => {
   justify-content: center;
   background-color: #ffc107;
   z-index: 2;
+
+  .skills-title {
+    padding-top: 30px;
+    height: 20%;
+    font-size: 5rem;
+    text-shadow:
+      1px 1px 1px #fff,
+      1px 2px 1px #fff,
+      1px 3px 1px #fff,
+      1px 4px 1px #fff,
+      1px 5px 1px #fff,
+      1px 6px 1px #fff,
+      1px 7px 1px #fff,
+      1px 8px 1px #fff,
+      1px 18px 6px rgba(16, 16, 16, 0.4),
+      1px 22px 10px rgba(16, 16, 16, 0.2),
+      1px 25px 35px rgba(16, 16, 16, 0.2),
+      1px 30px 60px rgba(16, 16, 16, 0.4);
+    font-weight: 900;
+  }
 
   .swiper-container {
     display: flex;
