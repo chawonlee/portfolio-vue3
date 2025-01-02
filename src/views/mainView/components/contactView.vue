4861f447b5fefd055a6f5935baf07042e36331df
chawonlee <ss01066865089@gmail.com>
2025-01-02 17:48:01 +0900

    [contact] 컴포넌트 추가

---
+++ src/views/mainView/components/contactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="contact-wrap"></section>
+</template>
+<script setup></script>
+<style lang="scss">
+.contact-wrap {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  z-index: 2;
+}
+</style>
