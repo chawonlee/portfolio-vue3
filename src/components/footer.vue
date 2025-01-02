a79c75ba95462bc1cf3d8aa1de3c9e1430a85bbd
chawonlee <ss01066865089@gmail.com>
2025-01-02 17:48:22 +0900

    [footer] 컴포넌트 추가

---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="footer-wrap">
+    © 2024. Lee Chae Won. All rights reserved.
+  </section>
+</template>
+<script setup></script>
+<style lang="scss">
+.footer-wrap {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+  color: white;
+  z-index: 2;
+}
+</style>
