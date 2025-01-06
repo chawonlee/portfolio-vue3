2d3bfae9063f91235fd269dbfde1dd509384bbed
chawonlee <ss01066865089@gmail.com>
2025-01-06 16:09:05 +0900

    [contact] title 추가

---
+++ src/components/statusIcon.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="status-icon" :class="isError ? 'error' : 'pass'">
+    <v-icon
+      :icon="isError ? 'mdi-alert-circle-outline' : 'mdi-check-bold'"
+    ></v-icon>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref, onMounted } from 'vue'
+
+onMounted(() => {})
+
+const props = defineProps({
+  isError: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<style scoped>
+.status-icon {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 25px !important;
+  height: 100%;
+  padding-right: 10px;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  color: #c1c3c5;
+  transition: all ease 0.4s;
+
+  &.pass {
+    color: #0374ff;
+  }
+
+  &.error {
+    color: #f96969;
+  }
+}
+</style>
--- src/views/mainView/components/contactView.vue
+++ src/views/mainView/components/contactView.vue
@@ -3,10 +3,57 @@
     <section class="contact-title">
       <div>CONTACT</div>
     </section>
-    <v-card color="#fff" class="contact-container"> </v-card>
+    <v-card color="#fff" class="contact-container">
+      <!-- Title -->
+      <div class="editor-wrap">
+        <label class="editor-label">Title *</label>
+        <div class="editor" :class="{ error: formError.title.error }">
+          <input
+            id="title"
+            type="text"
+            v-model="formMng.title"
+            required
+            @blur="titleVaild()"
+          />
+          <statusIcon :isError="formError.title.error"></statusIcon>
+          <span v-if="formError.title.error">{{
+            formError.title.errorMsg
+          }}</span>
+        </div>
+      </div>
+    </v-card>
   </section>
 </template>
-<script setup></script>
+<script setup>
+import statusIcon from '@/components/statusIcon.vue'
+import { ref, watch } from 'vue'
+
+const formError = ref({
+  title: {
+    error: true,
+    errorMsg: '제목을 입력해주세요',
+  },
+})
+const formMng = ref({
+  title: null,
+})
+// `title` 변경 감지
+watch(
+  () => formMng.value.title,
+  (newVal, oldVal) => {
+    titleVaild()
+  },
+)
+
+const titleVaild = () => {
+  if (!formMng.value.title || formMng.value.title.length < 1) {
+    formError.value.title.error = true
+    formError.value.title.errorMsg = '제목을 입력해주세요'
+  } else {
+    formError.value.title.error = false
+  }
+}
+</script>
 <style lang="scss">
 .contact-wrap {
   position: relative;
@@ -38,8 +85,6 @@
   }
 
   .contact-container {
-    align-items: center;
-    justify-content: center;
     width: 660px;
     height: 600px;
     border-radius: 10px; /* 모서리를 둥글게 */
@@ -48,13 +93,79 @@
       /* 부드러운 그림자 */ 0 1px 3px rgba(0, 0, 0, 0.06);
     border: 1px solid rgba(0, 0, 0, 0.1);
     transition: all 0.3s ease-in-out; /* hover 효과를 위한 트랜지션 */
-
+    padding: 15px 15px 15px 15px;
     &:hover {
       box-shadow:
         0 6px 8px rgba(0, 0, 0, 0.2),
         0 2px 4px rgba(0, 0, 0, 0.15);
       transform: translateY(-1px);
     }
+    .editor-wrap {
+      width: 100%;
+      .editor-label {
+        width: 100%;
+      }
+
+      .editor {
+        position: relative;
+        width: 100%;
+        height: 42px;
+        border-radius: 4px;
+        padding: 0.5rem;
+        background: #edf5fd;
+        transition: all 0.4s ease-in-out;
+        font-size: 8px;
+        outline: transparent;
+
+        &.disabled {
+          background-color: #ececec;
+        }
+        &.error {
+          border: 1px solid #f96969;
+          background: #fef7f7;
+          margin-bottom: 1.4rem;
+
+          &:has(:focus-visible) {
+            outline: #f96969 auto 1px;
+          }
+
+          span:not(.v-select__selection-text) {
+            position: absolute;
+            top: calc(100% + 4px);
+            left: 0;
+            color: #f96969;
+            font-size: 0.8rem;
+            font-weight: 400;
+          }
+        }
+
+        input {
+          width: 100%;
+          padding: 0;
+          border-radius: 0;
+          border: none;
+          background: transparent;
+          font-size: 1rem;
+          font-weight: 400;
+
+          &:focus-visible {
+            outline: none;
+          }
+        }
+      }
+    }
   }
+}
+</style>
+<style>
+.mdi:before,
+.mdi-set {
+  display: inline-block;
+  font: normal normal normal 24px / 1 'Material Design Icons';
+  font-size: 20px !important;
+  text-rendering: auto;
+  line-height: inherit;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
