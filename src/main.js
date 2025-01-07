6ab82c2cf27671461b73ca5ad8f8b4ead97893fb
chawonlee <ss01066865089@gmail.com>
2025-01-07 14:22:23 +0900

    [toastify] 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -12,11 +12,14 @@ import 'swiper/swiper-bundle.css' // swiper style
 
 /** ✏️ Vuetify Plugins (i18n, vuetify) */
 import vuetifyPlugins from '@/plugin/vuetify'
-
+/** ✏️ Push Alarm */
+import Vue3Toasity from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 const app = createApp(App)
 
 app.use(vuetifyPlugins.i18n)
 app.use(vuetifyPlugins.vuetify)
+app.use(Vue3Toasity)
 app.use(createPinia())
 app.use(router)
 
