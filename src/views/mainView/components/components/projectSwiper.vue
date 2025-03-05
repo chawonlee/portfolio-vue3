f9501630e48fbca92e80295e552c66db815e0d95
chawonlee <ss01066865089@gmail.com>
2025-03-05 14:05:00 +0900

    [projects] style 분리

--- src/views/mainView/components/components/projectSwiper.vue
+++ src/views/mainView/components/components/projectSwiper.vue
@@ -9,9 +9,9 @@
     :zoom="true"
     :thumbs="{ swiper: thumbsSwiper }"
     :modules="modules"
-    class="mySwiper2"
+    class="project-swiper"
   >
-    <swiper-slide v-if="props.project.videos">
+    <swiper-slide v-if="props.project.videos" class="project-swiper-slide">
       <video
         loop
         autoplay
@@ -20,7 +20,11 @@
         :src="`assets/projects/${props.project.id}/${props.project.videos}`"
       ></video>
     </swiper-slide>
-    <swiper-slide v-for="(img, idx) in props.project.images" :key="idx">
+    <swiper-slide
+      v-for="(img, idx) in props.project.images"
+      :key="idx"
+      class="project-swiper-slide"
+    >
       <img :src="`assets/projects/${props.project.id}/${img}`" />
     </swiper-slide>
   </swiper>
@@ -35,14 +39,18 @@
     :freeMode="true"
     :watchSlidesProgress="true"
     :modules="modules"
-    class="mySwiper"
+    class="project-swiper-video"
   >
-    <swiper-slide v-if="props.project.videos">
+    <swiper-slide v-if="props.project.videos" class="project-swiper-slide">
       <video
         :src="`assets/projects/${props.project.id}/${props.project.videos}`"
       ></video>
     </swiper-slide>
-    <swiper-slide v-for="(img, idx) in props.project.images" :key="idx">
+    <swiper-slide
+      v-for="(img, idx) in props.project.images"
+      :key="idx"
+      class="project-swiper-slide"
+    >
       <img :src="`assets/projects/${props.project.id}/${img}`" />
     </swiper-slide>
   </swiper>
@@ -69,3 +77,74 @@ const setThumbsSwiper = swiper => {
   thumbsSwiper.value = swiper
 }
 </script>
+<style scoped>
+.project-swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.project-swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.project-swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.swiper {
+  width: 100%;
+  height: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.project-swiper-slide {
+  background-size: cover;
+  background-position: center;
+}
+
+.project-swiper {
+  height: 100%;
+  width: 100%;
+}
+
+.project-swiper-video {
+  height: 100%;
+  box-sizing: border-box;
+  padding: 10px 0;
+}
+
+.project-swiper-video .swiper-slide {
+  width: 25%;
+  height: 100%;
+  opacity: 0.4;
+}
+
+.project-swiper-video .swiper-slide-thumb-active {
+  opacity: 1;
+}
+
+.project-swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.project-swiper-slide video {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
